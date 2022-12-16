1dfb4fbf0b9e45ec4d7e04b0a6c289e2b43a4f7d
ShariqAnsari88 <36728316+ShariqAnsari88@users.noreply.github.com>
2022-12-17 02:52:45 +0530

    remove unused files & code

--- src/makeRequest.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export const makeRequest = axios.create({
-    baseURL: process.env.REACT_APP_STRIPE_APP_DEV_URL,
-    headers: {
-        Authorization: "bearer " + process.env.REACT_APP_STRIPE_DEV_APP_KEY,
-    },
-});
